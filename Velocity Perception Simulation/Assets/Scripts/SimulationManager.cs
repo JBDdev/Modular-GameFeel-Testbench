55487bfefd3ea6109f45eebbb26b98f17d4dc10a
JBDdev <brodydavison4@gmail.com>
2023-02-20 00:02:12 -0500

    UI setup + fixes + basic file output

--- Velocity Perception Simulation/Assets/Scripts/SimulationManager.cs
+++ Velocity Perception Simulation/Assets/Scripts/SimulationManager.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.Timeline.Actions;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
 
 public class SimulationManager : MonoBehaviour
 {
     [SerializeField] GameObject player;
-    [SerializeField] float defaultSpeed;
+    [SerializeField] float maxSpeed;
     [SerializeField] float[] timerStartThresholds;
     [SerializeField] float timeout;
-    [SerializeField] float accelerationFactor;
+    [SerializeField] GameObject continueMenu;
+    [SerializeField] GameObject timeoutIndicator;
+    [SerializeField] GameObject testNumIndicator;
 
     int currentTest;
 
@@ -17,59 +23,101 @@ public class SimulationManager : MonoBehaviour
 
     [SerializeField] float elapsedTime;
     [SerializeField] float startTimer;
-    [SerializeField] bool accelerationTriggered;
+    [SerializeField] bool movementTriggered;
 
     Rigidbody rb;
     float currentSpeed;
 
     Vector3 startingPosition;
 
+    bool beginSimulation;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = player.GetComponent<Rigidbody>();
-        currentSpeed = defaultSpeed;
+        currentSpeed = 0;
         elapsedTime = 0;
         startTimer = 0;
-        accelerationTriggered = false;
+        movementTriggered = false;
         currentTest = 0;
         startingPosition = player.transform.position;
         results = new float[3];
+        beginSimulation = false;
+
+        //Generate random start values
+        for (int i = 0; i < timerStartThresholds.Length; i++)
+        {
+            timerStartThresholds[i] = Random.Range(2.5f, 15f);
+        }
+
+        GameObject canvas = GameObject.Find("Canvas");
+        continueMenu = canvas.transform.GetChild(0).gameObject;
+        testNumIndicator = canvas.transform.GetChild(1).gameObject;
+        timeoutIndicator = canvas.transform.GetChild(2).gameObject;
     }
 
 
     // Update is called once per frame
     private void Update()
     {
-        if (accelerationTriggered)
+        if (beginSimulation)
         {
-            elapsedTime += Time.deltaTime;
-        }
-        else 
-        {
-            startTimer += Time.deltaTime;
-            if(startTimer > timerStartThresholds[currentTest]) 
+            //Debug.Log(currentTest);
+            if (movementTriggered)
             {
-                accelerationTriggered = true;
+                elapsedTime += Time.deltaTime;
+            }
+            else
+            {
+                startTimer += Time.deltaTime;
+                if (startTimer > timerStartThresholds[currentTest])
+                {
+                    movementTriggered = true;
+                }
+            }
+
+            if (elapsedTime > timeout)
+            {
+                RecordResult();
+                timeoutIndicator.gameObject.SetActive(true);
+                Invoke("ClearTimeoutIndicator", 2f);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+                RecordResult();
+        }
+        else
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                continueMenu.SetActive(false);
+                testNumIndicator.SetActive(true);
+                Invoke("BeginRecordingData", 0.1f);
             }
         }
-
-        if (elapsedTime > timeout)
-            RecordResult();
-
-        if (Input.GetMouseButtonDown(0)) 
-            RecordResult();
         
+    }
+
+    void ClearTimeoutIndicator() 
+    {
+        timeoutIndicator.gameObject.SetActive(false);
+    }
+
+    void BeginRecordingData() 
+    {
+        beginSimulation = true;
     }
 
     void FixedUpdate()
     {
         rb.velocity = Vector3.forward * currentSpeed;
 
-        if (accelerationTriggered) 
+        if (movementTriggered) 
         {
-            currentSpeed += accelerationFactor;
+            currentSpeed += (timerStartThresholds[currentTest] / timeout) * Time.fixedDeltaTime;
+            if(currentSpeed > maxSpeed)
+                currentSpeed = maxSpeed;
         }
     }
 
@@ -80,10 +128,9 @@ public class SimulationManager : MonoBehaviour
         //Reset for the next test
         elapsedTime = 0;
         startTimer = 0;
-        currentSpeed = defaultSpeed;
+        currentSpeed = 0;
         player.transform.position = startingPosition;
-
-        currentTest++;
+        movementTriggered = false;
 
         if(currentTest == 2)
         {
@@ -92,6 +139,18 @@ public class SimulationManager : MonoBehaviour
                 Debug.Log("Result " + (i+1) + ": " + results[i]);
             }
             //Output to the file then load the next scene
+
+            using (StreamWriter streamWriter = new StreamWriter("output.txt", true)) 
+            {
+                streamWriter.WriteLine(SceneManager.GetActiveScene().name + " results: " + results[0] + ", " + results[1] + ", " + results[2]);
+            }
+
+            SceneManager.LoadScene("MainMenu");
         }
+
+        currentTest++;
+
+        testNumIndicator.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = "";
+        testNumIndicator.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = "" + (currentTest + 1);
     }
 }
